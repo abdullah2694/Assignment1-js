d267417c27087082457fbfec061b8ec38a9f5f2e
Abdullah Irfan <abdullah.irfan2609@gmail.com>
2025-02-14 17:54:29 +0500

    JS Assignment 1
    JS Assignment 1

---
+++ app.js
@@ -0,0 +1,106 @@
+// Chapter 1
+alert("Abdullah")
+alert("Irfan")
+alert("abdullah.irfan2609@gmail.com")
+alert("03300231292")
+alert("123456789")
+alert("You are learning JS")
+alert("I love JS")
+
+
+// Chapter 2
+// Q1
+let myVariableName = "Example";
+// Q2
+let myString;
+myString = "Hello, world!";
+// Q3
+let teamName = "Real Madrid";
+alert(teamName);
+// Q4
+var bestMan = "Charlie";
+bestMan = "Alex";
+
+// Chapter 3
+let caseQty;
+caseQty = 144;
+var num = "9";
+num = Number(num); 
+let totalAmount;
+totalAmount = 5 + 10;
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+orderTotal = 100 + 10; 
+// Result is 110
+let initialValue = 15;
+initialValue = initialValue + 25;
+
+
+// Alert
+alert("Welcome to our website!");
+alert("Error! Please enter a valid Password")
+alert("Welcome To JS Landing .... \n Happy Codding ")
+alert("Welcome To JS Landing ....")
+alert("Happy Codding")
+console.log("Hello!.. I can run js through my web console")
+
+
+// VARIABLES FOR STRINGS
+let username;
+let myName = "Abdullah Irfan";
+let message;
+message = "Hello World";
+alert(message);
+
+let studentName = "Abdullah Irfan";
+let studentAge = 20;
+let studentGrade = "A";
+let studentSchool = "DHA Suffa University";
+
+alert("Student Name: " + studentName);
+alert("Student Age: " + studentAge);
+alert("Student Grade: " + studentGrade);
+alert("Student School: " + studentSchool);
+
+alert("PIZZA \nPIZZ \nPIZ \n PI \n P")
+let email = "example@example.com";  
+alert("My email address is: " + email);
+
+let book = "A smarter way to learn JavaScript";
+alert("The book title is: " + book);
+
+document.write("The book title is: " + book);
+
+let specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(specialString);
+
+// VARIABLES FOR NUMBER
+let age = 20;
+alert("My age is: " + age);
+
+alert("you have vsited this site " +age+ "times")
+let birthYear = 2004;
+document.write("My Birth Year Is " +birthYear);
+
+let visitorName = "John Doe";
+let productTitle = "T-shirt";
+let quantity = 5;
+
+document.write (visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.")
+
+// VARIABLE NAMES: LEGAL & ILLEGAL
+document.write("<p>Variable names can only contain letters, numbers, $ and _. For example: <b>$my_1stVariable</b></p>");
+document.write("<p>Variables must begin with a letter, $ or _. For example: <b>$name</b>, <b>_name</b>, or <b>name</b></p>");
+document.write("<p>Variable names are case-sensitive</p>");
+document.write("<p>Variable names should not be JS keywords</p>");
+
+var firstName = "John", lastName = "Doe", age = 25;
+
+var myVar;
+var _name;
+var $price;
+var user123;
+var camelCaseExample;
+
+console.log("Variables declared successfully.");
